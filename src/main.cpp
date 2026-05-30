43e70d337297e4e9bc92eed82fa84f44c9045433
XeniaTklkn <tukalkinaksenia@gmail.com>
2026-05-30 14:01:53 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
 #include "bst.h"
 
-int main() {
+void makeTree(BST<std::string>& tree,
+              const char* filename);
+void printFreq(BST<std::string>& tree);
+int main()
+{
+    BST<std::string> tree;
+    makeTree(tree, "src/war_peace.txt");
+    std::cout << "Depth = "
+              << tree.depth()
+              << std::endl;
+    std::cout << "Pierre = "
+              << tree.search("pierre")
+              << std::endl;
+    printFreq(tree);
     return 0;
 }
